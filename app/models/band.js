ecd151880a32dd9c3df798f213e0b0523abc778f
MarkhamShofner <markham.shofner@gmail.com>
2016-04-22 17:20:00 -0400

    modify band model

--- app/models/band.js
+++ app/models/band.js
@@ -1,12 +1,10 @@
 import Ember from 'ember';
-export default Ember.Object.extend({
-  name: '',
-  description: '',
-  setupSongs: Ember.on('init', function() {
-    if (!this.get('songs')) {
-      this.set('songs', []);
-    }
-  }),
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  description: DS.attr(),
+  songs: DS.hasMany('song'),
   slug: Ember.computed('name', function() {
     return this.get('name').dasherize();
   }),
