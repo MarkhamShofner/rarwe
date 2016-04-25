34fe60b9be5ce2973d4e8e58c313a96b4429de22
MarkhamShofner <markham.shofner@gmail.com>
2016-04-25 16:49:14 -0400

    incorporate pretender package to provide fake data, and use that provided faker data to pass the previously written acceptance test

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,9 +1,37 @@
-import { test } from 'qunit';
+import {
+  test
+} from 'qunit';
 import moduleForAcceptance from 'rarwe/tests/helpers/module-for-acceptance';
+import Pretender from 'pretender';
 
 moduleForAcceptance('Acceptance | bands');
 
+var server;
+
 test('visiting /bands', function(assert) {
+  server = new Pretender(function() {
+    this.get('/bands', function() {
+      var response = {
+        data: [{
+          id: 1,
+          type: 'bands',
+          attributes: {
+            name: 'Radiohead'
+          }
+        }, {
+          id: 2,
+          type: 'bands',
+          attributes: {
+            name: 'Long Distance Calling'
+          }
+        }, ]
+      };
+      return [200, {
+        'Content-Type': 'application/vnd.api+json'
+      }, JSON.stringify(response)];
+    });
+  });
+
   visit('/bands');
 
   andThen(function() {
