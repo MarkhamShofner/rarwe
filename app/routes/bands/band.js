cbfde71999b8cc0d0158aad9fe8906450d90ddf1
MarkhamShofner <markham.shofner@gmail.com>
2016-04-25 13:27:43 -0400

    modify files to allow loading of data from jsonapi, moving onto saving to the backend

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -1,11 +1,10 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({});
-
 export default Ember.Route.extend({
   model: function(params) {
-    var bands = this.modelFor('bands');
-    return bands.get('content').findBy('slug', params.slug); // params.slug is now 'pearl-jam'
+    return this.store.findRecord('band', params.id);
+    // var bands = this.modelFor('bands');
+    // return bands.get('content').findBy('slug', params.slug); // params.slug is now 'pearl-jam'
   },
 
   afterModel: function (band){
